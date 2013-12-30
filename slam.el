b0f39dc1101e4e62575db18fd8d9603413a69a35
Leslie Wu <lwu2@cs.stanford.edu>
2013-12-30 07:42:38 -0800

    Add slam.el

---
+++ slam.el
@@ -0,0 +1,14 @@
+(fset 'copy-character-from-other-buffer
+   [?\C-x ?5 ?o ?\C-  right escape ?w ?\C-x ?5 ?o ?\C-y])
+
+(global-set-key [(a)] 'copy-character-from-other-buffer)
+(global-set-key [(s)] 'copy-character-from-other-buffer)
+(global-set-key [(d)] 'copy-character-from-other-buffer)
+(global-set-key [(f)] 'copy-character-from-other-buffer)
+(global-set-key [(g)] 'copy-character-from-other-buffer)
+(global-set-key [(h)] 'copy-character-from-other-buffer)
+(global-set-key [(j)] 'copy-character-from-other-buffer)
+(global-set-key [(j)] 'copy-character-from-other-buffer)
+(global-set-key [(k)] 'copy-character-from-other-buffer)
+(global-set-key [(l)] 'copy-character-from-other-buffer)
+(global-set-key [(\;)] 'copy-character-from-other-buffer)
